8c12a903dea1a30cb509f6d0bca68b534e2228c1
dralagen <dralagen@dralagen.fr>
2025-04-11 15:56:58 +0200

    disabled form when sending request

--- messasync-chat/src/app/message-writer/message-writer.component.ts
+++ messasync-chat/src/app/message-writer/message-writer.component.ts
@@ -1,29 +1,47 @@
-import {Component, inject} from '@angular/core';
-import {FormsModule, NgForm} from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Message, MessageService} from '../message.service';
 
 @Component({
   selector: 'app-message-writer',
   imports: [
-    FormsModule
+    ReactiveFormsModule
   ],
   templateUrl: './message-writer.component.html',
   styleUrl: './message-writer.component.css'
 })
-export class MessageWriterComponent {
+export class MessageWriterComponent implements OnInit {
+  messageForm!: FormGroup; // Added "!" non-null assertion operator
+  private sendMessageService = inject(MessageService);
+  private fb = inject(FormBuilder);
 
-  private sendMessageService = inject(MessageService)
-
-  sendMessage(form: NgForm) {
-    if (form.valid) {
-      console.log("Sending message", form.value.message);
-      let message = new Message(form.value.message, "default")
-      this.sendMessageService.sendMessage(message)
-        .subscribe(() => {
-          form.reset();
-        });
-
-    }
+  ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm(): void {
+    this.messageForm = this.fb.group({
+      message: ['', Validators.required]
+    });
+  }
+
+  sendMessage(): void {
+    if (this.messageForm.valid) {
+      this.messageForm.disable()
+      console.log("Sending message", this.messageForm.value.message);
+      const message = new Message(this.messageForm.value.message, "default");
+
+      this.sendMessageService.sendMessage(message)
+        .subscribe({
+          next: () => {
+            this.messageForm.reset();
+            this.messageForm.enable();
+          },
+          error: (error) => {
+            console.error('Error sending message:', error);
+            this.messageForm.enable()
+          }
+        });
+    }
+  }
 }
